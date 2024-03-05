492ad1a5d5de676e3b39324494069c48e28e6425
Tom Prezioso <73192767+TheKarmicChaos@users.noreply.github.com>
2024-03-05 18:32:10 -0500

    Update imports

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from lib import initDB, insertMsg
+from lib import *
+from bot import *
 
-# con = Connection Obj
-# cur = Cursor Obj
 (con, cur) = initDB()
 table = cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name = 'Message';")
 print(cur.fetchone())
