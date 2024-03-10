75e746ca54f76222c1cf550793b4b3feb9e569e1
Tom Prezioso <73192767+TheKarmicChaos@users.noreply.github.com>
2024-03-09 20:30:51 -0500

    Split existing function into two
    initBot and getConfig are now 2 separate funcs so that client classes can use getConfig later.

--- bot.py
+++ bot.py
@@ -3,19 +3,31 @@
 import discord as dc
 import json
 
-def initBot():
-    """Sets up bot using config.json and connects it to your server so you can use it."""
+
+def getConfig():
+    """Reads config from file and returns a config dict"""
     config = {}
-    # Read config from file
     with open('config.json', 'r') as f:
         config = json.loads(f.read())
         f.close()
+    # convert IDs to ints
+    config['guildID'] = int(config['guildID'])
+    channelIDs = []
+    for channel in config['channelIDs']:
+        channelIDs.append(int(channel))
+    config['channelIDs'] = channelIDs
+    return config
 
-    try: # Try to construct config dict from the read file
-        intents = dc.Intents.default()
-        intents.message_content = True
 
-        client = MyClient(intents=intents)
+def initBot(client):
+    """Sets up a bot client using config.json and connects it to your server so you can use it.
+    
+    Args:
+        client (Client): Discord client connection object with the corresponding code & intents for the desired behavior of the bot.
+    """
+    
+    config = getConfig()
+    try:
         client.run(config["botToken"])
     except:
         print("Could not load config.json properly. Perhaps you forgot to replace the placeholders?")
