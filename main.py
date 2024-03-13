b7a4cbb55e3baf7a721bedc950ca6512de3a4aab
Tom Prezioso <73192767+TheKarmicChaos@users.noreply.github.com>
2024-03-13 14:54:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from lib import *
 from bot import *
 
+# temp test code for running a bot
+intents = dc.Intents.default()
+intents.message_content = True
+
+client = ChatClient(intents=intents)
+initBot(client)
+
+# temp test code for executing SQL queries
 (con, cur) = initDB()
 res = cur.execute("SELECT COUNT(*) FROM Message")
 print(cur.fetchall())
