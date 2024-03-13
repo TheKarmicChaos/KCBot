ead2e55b80c0eaea63f5e0ab823820159f07e89d
Tom Prezioso <73192767+TheKarmicChaos@users.noreply.github.com>
2024-03-13 15:00:54 -0400

    Update test code in main.py
    Temp test code now split into separate funcs.

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from lib import *
 from bot import *
 
-# temp test code for running a bot
-intents = dc.Intents.default()
-intents.message_content = True
+# temp test code for running chat bot
+def runChatBot():
+    intents = dc.Intents.default()
+    intents.message_content = True
 
-client = ChatClient(intents=intents)
-initBot(client)
+    client = ChatClient(intents=intents)
+    initBot(client)
+
+# temp test code for running scrape bot
+def runScrapeBot():
+    intents = dc.Intents.default()
+    intents.message_content = True
+
+    client = ScrapeClient(intents=intents)
+    initBot(client)
 
 # temp test code for executing SQL queries
-(con, cur) = initDB()
-res = cur.execute("SELECT COUNT(*) FROM Message")
-print(cur.fetchall())
+def querySQL(query):
+    (con, cur) = initDB()
+    res = cur.execute(query)
+    print(cur.fetchall())
+
+querySQL("SELECT COUNT(*) FROM Message")
