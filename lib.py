5b58191454f5bc3acf442573ceab267f88b614c1
Tom Prezioso <73192767+TheKarmicChaos@users.noreply.github.com>
2024-03-20 20:56:06 -0400

    Merged bot.py into lib.py

--- lib.py
+++ lib.py
@@ -1,6 +1,123 @@
 import sqlite3
 import os
+import json
+import discord as dc
+# Kaycee bot requires the 'message_content' intent to be enabled.
 
+def getConfig():
+    """Reads config from file and returns a config dict"""
+    config = {}
+    with open('config.json', 'r') as f:
+        config = json.loads(f.read())
+        f.close()
+    # convert IDs to ints
+    config['guildID'] = int(config['guildID'])
+    config['botID'] = int(config['botID'])
+    channelIDs = []
+    for channel in config['channelIDs']:
+        channelIDs.append(int(channel))
+    config['channelIDs'] = channelIDs
+    return config
+
+
+def initBot(client):
+    """Sets up a bot client using config.json and connects it to your server so you can use it.
+    
+    Args:
+        client (Client): Discord client connection object with the corresponding code & intents for the desired behavior of the bot.
+    """
+    
+    config = getConfig()
+    try:
+        client.run(config["botToken"])
+    except:
+        print("Could not load config.json properly. Perhaps you forgot to replace the placeholders?")
+
+
+class ChatClient(dc.Client):
+    """Discord bot client for sending and responding to chat messages.
+    When activated with initBot() it will respond to messages starting with "/kc"
+    in the the channels specified in config.json.
+    """
+    config = getConfig()
+    guildID = config["guildID"]
+    channelIDs = config["channelIDs"]
+    
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print(f'Guild: "{self.get_guild(self.guildID)}"')
+        print('------')
+        print(f'Chatbot enabled in the following channels:')
+        for channel in self.channelIDs:
+            print(self.get_channel(channel))
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('/kc '):
+            prompt = message.content[4:]
+            (con, cur) = initDB()
+            # TODO: Run the prompt through AI and send the result as a discord message.
+            result = cur.execute(prompt)
+            # Placeholder for when AI part of project is finished.
+            await message.reply(result.fetchmany(10), mention_author=True)
+
+
+class ScrapeClient(dc.Client):
+    """Discord bot client for scraping messages.
+    When activated with initBot() it will automatically scrape the channels specified in config.json.
+    """
+    config = getConfig()
+    guildID = config["guildID"]
+    channelIDs = config["channelIDs"]
+    
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print(f'Guild: "{self.get_guild(self.guildID)}"')
+        print('------')
+        
+        print('Connecting to database...')
+        (con, cur) = initDB()
+        print('Channels to scrape:')
+        for channel in self.channelIDs:
+            print(f'\t{self.get_channel(channel)}')
+        print('------')
+        
+        for channel in self.channelIDs:
+            chan = self.get_channel(channel)
+            print(f'({chan}) Fetching most recent msg in db...')
+            # Fetch most recent message in db for this channel
+            mostRecentMsg = getMostRecent(con, cur, channel)
+            if mostRecentMsg != None:
+                mostRecentMsg = await chan.fetch_message(mostRecentMsg)
+            msgBatch = []
+            count = 0
+            print(f'({chan}) Scraping message history (this may take a while)...')
+            async for message in chan.history(limit=10000, after=mostRecentMsg, oldest_first=True):
+                # check if this message references another message.
+                reference = None
+                try:
+                    reference = message.reference.message_id
+                except:
+                    reference = None
+                msg = (message.id,
+                        message.channel.id,
+                        message.channel.name,
+                        message.author.id,
+                        message.author.name,
+                        str(message.created_at),
+                        message.content,
+                        reference)
+                msgBatch.append(msg)
+                count += 1
+            print(f'({chan}) Saving {count} messages to db...')
+            insertMsg(con, cur, msgBatch)
+            print(f'({chan}) Done!')
+        print('------')
+        
 
 def initDB():
     """
