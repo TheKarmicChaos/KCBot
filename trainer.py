8e6025ce36e4427af0b402a35a567853da21709c
Tom Prezioso <73192767+TheKarmicChaos@users.noreply.github.com>
2024-04-12 15:19:50 -0400

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,6 +1,6 @@
 from lib import *
 from datasets import load_dataset
-from trl import SFTTrainer
+import trl
 import json
 import transformers
 
@@ -9,7 +9,13 @@ import transformers
 
 model = transformers.AutoModelForCausalLM.from_pretrained("facebook/opt-350m")
 tokenizer = transformers.AutoTokenizer.from_pretrained("facebook/opt-350m")
-tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+#tokenizer.add_special_tokens({'pad_token': '<pad>'})
+
+# Makes loss calculations ignore the "### Repsonse:" label
+response_template = """### Response:
+"""
+collator = trl.DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
+
 
 def formatting_prompts_func(examples):
     output_text = []
@@ -38,11 +44,12 @@ with open('db' + os.sep + 'input_output_dataset.json', 'w') as f:
     json.dump(trainingData, f)
     f.close()
 dataset = load_dataset("json", data_files = "db" + os.sep + "input_output_dataset.json")
-
-# Split our dataset into a 80-20% split for training/evaluation respectively. This is randomized each time we resume training.
+# Split our dataset into a 80-20% split for training/evaluation respectively.
 split_dataset = dataset['train'].train_test_split(test_size=0.2, seed=42)
 print(split_dataset)
 print("-----")
+
+
 
 trainingArgs = transformers.TrainingArguments(
     output_dir= './tmp_trainer',
@@ -51,10 +58,11 @@ trainingArgs = transformers.TrainingArguments(
     save_total_limit = 10,          # Saves 10 most recent checkpoints before deleting oldest. Checkpoints are big files, but you can increase this number if you have enough space.
     save_strategy = "epoch",        # Save a checkpoint of the model at the end of each epoch.
     evaluation_strategy = "epoch",  # Evaluate the model at the end of each epoch. This gives us a loss value to determine which checkpoint is the current best.
-    logging_strategy = "epoch"      # Make a log at the end of each epoch.
+    logging_strategy = "epoch",      # Make a log at the end of each epoch.
+    weight_decay = 0.1,
 )
 
-trainer = SFTTrainer(
+trainer = trl.SFTTrainer(
     model,
     args = trainingArgs,
     tokenizer = tokenizer,
@@ -63,14 +71,15 @@ trainer = SFTTrainer(
     formatting_func = formatting_prompts_func,
     max_seq_length = 512,
     packing = False,
-    dataset_batch_size = 16
+    dataset_batch_size = 16,
+    data_collator = collator,
 )
 
 # You can turn this on if your GPU is CUDA-enabled. Only do this if you have a GPU with more memory than your CPU (or a lot of GPUs).
 # Be sure to first install pytorch with CUDA via the instructions at https://pytorch.org/get-started/locally/
 # model.cuda()
-
 print("-----")
+
 try:    # Try to resume training from an existing checkpoint
     print("Attempting to resume training from checkpoint.")
     trainer.train(resume_from_checkpoint=True)
