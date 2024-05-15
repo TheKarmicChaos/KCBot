369b4b8db5d2772a32b2e781a9e09e1d342ff89d
Tom Prezioso <73192767+TheKarmicChaos@users.noreply.github.com>
2024-05-15 09:10:30 -0400

    Add cross-validation to training

--- trainer.py
+++ trainer.py
@@ -38,52 +38,64 @@ def formatting_prompts_func(examples):
 #TODO: This is super inefficient. Maybe I can simplify it?
 (con, cur) = initDB()
 trainingData = generateTrainingData(con, cur)
+dataset_loc = 'db' + os.sep + 'input_output_dataset.json'
 # Save this to a json file, then immediately load it for SFTTrainer
-with open('db' + os.sep + 'input_output_dataset.json', 'w') as f:
+with open(dataset_loc, 'w') as f:
     json.dump(trainingData, f)
     f.close()
-dataset = load_dataset("json", data_files = "db" + os.sep + "input_output_dataset.json")
 
-# Split our dataset into a 80-20% split for training/evaluation respectively.
-split_dataset = dataset['train'].train_test_split(test_size=0.2, seed=42)
-print(split_dataset)
+dataset = load_dataset("json", data_files = dataset_loc)
+
+# Manually split our dataset into 10 distinct 90-10% splits for training/evaluation respectively. This will let us manually cross-validate.
+split_dataset = dataset['train'].train_test_split(test_size=0.2)
+val_ds = load_dataset("json", data_files = dataset_loc, split=[f"train[{k}%:{k+10}%]" for k in range(0, 100, 10)])
+train_ds = load_dataset("json", data_files = dataset_loc, split=[f"train[:{k}%]+train[{k+10}%:]" for k in range(0, 100, 10)])
 print("-----")
-
 
 trainingArgs = transformers.TrainingArguments(
     output_dir= './tmp_trainer',
-    num_train_epochs = 100,         # Train for 100 epochs, after which we will consider our best checkpoint to be the final trained model. Increase/decrease this as needed.
+    num_train_epochs = 10,          # Train for 10 epochs, after which we will consider our best checkpoint to be the final trained model. Increase/decrease this as needed.
     load_best_model_at_end = True,  # Best checkpoint is always saved (counts toward total save limit defined below)
     save_total_limit = 10,          # Saves 10 most recent checkpoints before deleting oldest. Checkpoints are big files, but you can increase this number if you have enough space.
     save_strategy = "epoch",        # Save a checkpoint of the model at the end of each epoch.
-    evaluation_strategy = "epoch",  # Evaluate the model at the end of each epoch. This gives us a loss value to determine which checkpoint is the current best.
-    logging_strategy = "epoch",      # Make a log at the end of each epoch.
-    weight_decay = 0.05,
+    evaluation_strategy = "epoch",  # Evaluate the model at the end of each epoch. This gives us an eval_loss value to determine which checkpoint is the current best.
+    logging_strategy = "epoch",     # Make a log at the end of each epoch.
+    weight_decay = 0.001,
 )
 
-trainer = trl.SFTTrainer(
-    model,
-    args = trainingArgs,
-    tokenizer = tokenizer,
-    train_dataset = split_dataset['train'],
-    eval_dataset = split_dataset['test'],
-    formatting_func = formatting_prompts_func,
-    max_seq_length = 1024,
-    packing = False,
-    dataset_batch_size = 16,
-    data_collator = collator,
-)
+# iterate through our cross-validation dataset splits
+
+for trainerNum, train_dataset, val_dataset in zip(range(10), train_ds, val_ds):
+    print(f"Running Trainer {trainerNum+1}")
+    trainer = trl.SFTTrainer(
+        model,
+        args = trainingArgs,
+        tokenizer = tokenizer,
+        train_dataset = train_dataset,
+        eval_dataset = val_dataset,
+        formatting_func = formatting_prompts_func,
+        max_seq_length = 1024,
+        packing = False,
+        dataset_batch_size = 16,
+        data_collator = collator,
+        callbacks=[transformers.EarlyStoppingCallback(early_stopping_patience=2)],  # Stop training with this dataset split if the eval_loss gets worse for n epochs.
+    )
+    trainer.train()
+    trainer.save_model("model.bin")
+    print(f"Trainer {trainerNum+1}/10 complete.")
+    # At this point, our best model for that dataset split has been saved to "model.bin", so load that as the starting model for our next trainer
+    model = "model.bin"
 
 # You can turn this on if your GPU is CUDA-enabled. Only do this if you have a GPU with more memory than your CPU (or a lot of GPUs).
 # Be sure to first reinstall pytorch with CUDA via the instructions at https://pytorch.org/get-started/locally/
 # model.cuda()
 print("-----")
 
-try:    # Try to resume training from an existing checkpoint
-    print("Attempting to resume training from checkpoint.")
-    trainer.train(resume_from_checkpoint=True)
-except: # If no existing checkpoint exists, start training from scratch
-    print("WARNING - No existing checkpoint found. Training new model from scratch.")
-    trainer.train()
+#try:    # Try to resume training from an existing checkpoint
+#    print("Attempting to resume training from checkpoint.")
+#    trainer.train(resume_from_checkpoint=True)
+#except: # If no existing checkpoint exists, start training from scratch
+#    print("WARNING - No existing checkpoint found. Training new model from scratch.")
+#    trainer.train()
 
-trainer.save_model("model.bin")
+#trainer.save_model("model.bin")
